b8a69675b2dcba43390915bdcba0ed306306c9f8
cyrelonx <cyrelonx@proton.me>
2025-09-21 02:01:52 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+using namespace std;
+
+// Link with ws2_32.lib
+#pragma comment(lib, "ws2_32.lib")
+
+int main()
+{
+    int port;
+    cout << "Enter port number: ";
+    cin >> port;
+    
+    string ip_adrr;
+    cout << "Enter IP address: ";
+    cin >> ip_adrr;
+    cout << "Port: " << port << ", IP Address: " << ip_adrr << endl;
+
+    WSAData wsaData;
+    int startup = WSAStartup(MAKEWORD(2, 2), &wsaData);
+                        // ipv4   // tcp conn
+
+    if (!startup)
+    {
+        cout << "Error: " << startup << "\n";
+    }
+
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    sockaddr_in target;
+    target.sin_family = AF_INET;
+    target.sin_port = htons(port);
+    inet_pton(AF_INET, ip_adrr.c_str(), &target.sin_addr);
+
+    // Estabilish connection
+    int res = connect(sock, (sockaddr*)&target, sizeof(target));
+    if (res == SOCKET_ERROR)
+    {
+        int err = WSAGetLastError();
+        if (err == WSAECONNREFUSED || err == WSAETIMEDOUT)
+            cout << "Port is closed or filtered (err: " << err << ")!\n";
+        else
+            cout << "Connection failed due to an error: " << err << "\n";
+    }
+    else
+        cout << "Port is open!\n";
+
+    closesocket(sock);
+    WSACleanup();
+    return 0;
+}
